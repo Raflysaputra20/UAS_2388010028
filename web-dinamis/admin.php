beb66e18dc6c14b0ab0d5d4b6f41ca96f46b2aaa
EsBalok <raplesaputra2011@gmail.com>
2026-06-08 10:30:31 +0700

    NAMA - NIM

--- web-dinamis/admin.php
+++ web-dinamis/admin.php
@@ -75,7 +75,7 @@ if ($pkg_res) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Panel | VALOSTORE</title>
+    <title>MUHAMMADRAFLYSAPUTRA-2388010028 | VALOSTORE</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
